d2eba9606fbdebb590ad818aef8bb60c76e024d5
Lana Holmes <lana.lee.holmes@gmail.com>
2018-11-17 17:51:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-
+  var newObject = {object};
+  delete newObject[key];
+  return newObject;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
